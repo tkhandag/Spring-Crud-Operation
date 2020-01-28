c71a432450ab5420a4e31aa5a8c956ac3504d8a6
Khandagare <841698@cognizant.com>
2020-01-28 14:52:10 +0530

    f

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@Controller
+public class HomeController {
+
+		@RequestMapping("/home")
+		public ModelAndView home(DemoModel d) {
+			ModelAndView mv=new ModelAndView();
+			mv.addObject("obj",d);
+			
+			mv.setViewName("home");
+		
+			return mv;
+		}
+
+	
+
+}
